0ae64463a4e69ad3049c6cc87ffb492125c46921
老秦 <50172189@qq.com>
2020-05-21 12:34:54 +0800

    up

---
+++ src/Helpers/Helper.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Ericjank\Hutils\Helpers;
+
+use Ericjank\Hutils\Helpers\Code;
+
+Class Helper
+{
+
+    //返回成功
+    public function success($data)
+    {
+        return $this->result(Code::SUCCESS, Code::getMessage(Code::SUCCESS), $data);
+    }
+
+    //返回错误
+    public function error($code = 422, $message = '', $data = [])
+    {
+        if (empty($message)) {
+            return $this->result($code, Code::getMessage($code), $data);
+        } else {
+            return $this->result($code, $message, $data);
+        }
+    }
+
+    public function result($code, $message, $data)
+    {
+        return ['code' => $code, 'message' => $message, 'data' => $data];
+    }
+
+    public function jsonEncode($data)
+    {
+        return json_encode($data, JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * 生成随机数
+     * @param number $length
+     * @return number
+     */
+    public function generateNumber($length = 6)
+    {
+        return rand(pow(10, ($length - 1)), pow(10, $length) - 1);
+    }
+
+    /**
+     * 生成随机字符串
+     * @param number $length
+     * @param string $chars
+     * @return string
+     */
+    public function generateString($length = 6, $chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
+    {
+        $chars = str_split($chars);
+
+        $chars = array_map(function($i) use($chars) {
+            return $chars[$i];
+        }, array_rand($chars, $length));
+
+        return implode($chars);
+    }
+
+    /**
+     * xml to array 转换
+     * @param type $xml
+     * @return type
+     */
+    public function xml2array($xml)
+    {
+        return json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+    }
+
+    /**
+     * 第三方登录类型
+     *
+     * @param int $type 类型
+     * @return string
+     */
+    public function getThirdType($type)
+    {
+        switch ($type) {
+            case 1:
+                $thirdType = 'Weixin';
+                break;
+            case 2:
+                $thirdType = 'QQ';
+            case 3:
+                $thirdType = 'Weibo';
+                break;
+            default :
+                $thirdType = 'Weixin';
+        }
+
+        return $thirdType;
+    }
+
+}
---
+++ src/Helpers/Validation.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Ericjank\Hutils\Helpers;
+
+class Validation
+{
+    //手机号
+    public function isPhoneNumber(string $phone): bool
+    {
+        return preg_match("/^1[345678]{1}\d{9}$/",$phone) ? true : false;
+    }
+
+    //6-16位数字或字母密码
+    public function isPassword(string $password): bool
+    {
+        return preg_match("/^[0-9A-Za-z]{6,16}$/",$password) ? true : false;
+    }
+
+    //6位支付密码
+    public function isPaypwd(string $payPwd): bool
+    {
+        return preg_match("/^[0-9]{6}$/", $payPwd) ? true : false;
+    }
+
+    //身份证号
+    public function isIdCard(string $idCard): bool
+    {
+        $length = strlen($idCard);
+        if($length == 18){
+            return preg_match("/^[1-9]\d{5}(18|19|20|21|22|23)\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/", $idCard) ? true : false;
+        }elseif ($length == 15){
+            return preg_match("/^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}[0-9Xx]$/", $idCard) ? true : false;
+        }else{
+            return false;
+        }
+
+    }
+
+    //姓名
+    public function isChineseName(string $chineseName): bool
+    {
+        return preg_match("/^[\x{4e00}-\x{9fa5}]{1}[a-zA-Z\x{4e00}-\x{9fa5}·•]{1,7}$/u", $chineseName) ? true : false;
+    }
+
+}
